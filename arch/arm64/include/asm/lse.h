0c52d48751201ddf27e8d1ecda0846021fcd8e4b
Sami Tolvanen <samitolvanen@google.com>
2019-10-31 12:57:05 -0700

    arm64: lse: fix LSE atomics with LLVM's integrated assembler
    commit e0d5896bd356cd577f9710a02d7a474cdf58426b upstream.
    
    Unlike gcc, clang considers each inline assembly block to be independent
    and therefore, when using the integrated assembler for inline assembly,
    any preambles that enable features must be repeated in each block.
    
    This change defines __LSE_PREAMBLE and adds it to each inline assembly
    block that has LSE instructions, which allows them to be compiled also
    with clang's assembler.
    
    Link: https://github.com/ClangBuiltLinux/linux/issues/671
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>
    Tested-by: Andrew Murray <andrew.murray@arm.com>
    Tested-by: Kees Cook <keescook@chromium.org>
    Reviewed-by: Andrew Murray <andrew.murray@arm.com>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Reviewed-by: Nick Desaulniers <ndesaulniers@google.com>
    Signed-off-by: Will Deacon <will@kernel.org>
    [nd: backport adjusted due to missing:
      commit addfc38672c7 ("arm64: atomics: avoid out-of-line ll/sc atomics")]
    Signed-off-by: Nick Desaulniers <ndesaulniers@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Change-Id: I3829aee041ceb39c4252df262be93dde0ca7c156
    [fiqri19102002: Adapt with commit 95dda4fa4c41a14f87ce517da726b9b0b725ecf7]
    Signed-off-by: Fiqri Ardyansyah <fiqri0927936@gmail.com>

--- arch/arm64/include/asm/lse.h
+++ arch/arm64/include/asm/lse.h
@@ -4,6 +4,8 @@
 
 #if defined(CONFIG_AS_LSE) && defined(CONFIG_ARM64_LSE_ATOMICS)
 
+#define __LSE_PREAMBLE	".arch armv8-a+lse\n"
+
 #include <linux/compiler_types.h>
 #include <linux/export.h>
 #include <linux/stringify.h>
@@ -20,8 +22,6 @@
 
 #else	/* __ASSEMBLER__ */
 
-__asm__(".arch_extension	lse");
-
 /* Move the ll/sc atomics out-of-line */
 #define __LL_SC_INLINE		notrace
 #define __LL_SC_PREFIX(x)	__ll_sc_##x
@@ -32,7 +32,7 @@ __asm__(".arch_extension	lse");
 #define __LL_SC_CLOBBERS	"x30"
 
 /* In-line patching at runtime */
-#define ARM64_LSE_ATOMIC_INSN(llsc, lse)	lse
+#define ARM64_LSE_ATOMIC_INSN(llsc, lse)	__LSE_PREAMBLE lse
 
 #endif	/* __ASSEMBLER__ */
 #else	/* CONFIG_AS_LSE && CONFIG_ARM64_LSE_ATOMICS */
