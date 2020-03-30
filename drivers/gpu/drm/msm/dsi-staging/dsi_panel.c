627d93bd56c7321b291fb41d57633d13b5defdbc
Nauval Rizky <enuma.alrizky@gmail.com>
2020-03-30 21:46:09 +0900

    drm: exposure_adjustment: reduce usage of ifdef
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Change-Id: I04aced6835962b5075d024937cb3a0688235a82c
    Signed-off-by: Fiqri Ardyansyah <fiqri0927936@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -27,9 +27,7 @@
 #include "dsi_display.h"
 #endif
 
-#ifdef CONFIG_EXPOSURE_ADJUSTMENT
 #include "exposure_adjustment.h"
-#endif
 
 /**
  * topology is currently defined by a set of following 3 values:
@@ -795,10 +793,8 @@ int dsi_panel_set_backlight(struct dsi_panel *panel, u32 bl_lvl)
 
 	pr_debug("backlight type:%d lvl:%d\n", bl->type, bl_lvl);
 
-#ifdef CONFIG_EXPOSURE_ADJUSTMENT
         if (bl_lvl > 0)
                 bl_lvl = ea_panel_calc_backlight(bl_lvl < bl_dc_min ? bl_dc_min : bl_lvl);
-#endif
 
 	switch (bl->type) {
 	case DSI_BACKLIGHT_WLED:
