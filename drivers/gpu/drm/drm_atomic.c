d33f56a70abcbcedc95e705298362d25c82a6983
Cyber Knight <cyberknight755@gmail.com>
2021-09-30 21:21:12 +0800

    drm/atomic: Boost CPU & DDR bus according to set kernel profile
    Level 1 (battery) = 0 boost
    Level 2 (balanced) = boost to max freqs for 25ms
    Level 0 (disabled) / Level 3 (performance) = boost to max freqs for 50ms
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    [fiqri19102002: Adapt to our usage:
    		Level 1 (battery) = 0 boost
    		Level 2 (balanced) / Level 0 (disabled) = default boost with timeout
    		Level 3 (performance) = boost to max freqs for 100 ms and skip timeout boost
    		Adapt for new implementation and migrate to switch statement]
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -2207,6 +2207,8 @@ static void complete_crtc_signaling(struct drm_device *dev,
 	kfree(fence_state);
 }
 
+extern int kp_active_mode(void);
+
 static int __drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
 				   struct drm_file *file_priv)
 {
@@ -2251,11 +2253,22 @@ static int __drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
 		return -EINVAL;
 
 	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY)) {
-		if (cpu_input_boost_within_input(3250))
-			cpu_input_boost_kick();
+		switch (kp_active_mode()) {
+		case 0:
+		case 2:
+			if (cpu_input_boost_within_input(3250))
+				cpu_input_boost_kick();
 
-		if (df_boost_within_input(3250))
-			devfreq_boost_kick(DEVFREQ_CPU_LLCC_DDR_BW);
+			if (df_boost_within_input(3250))
+				devfreq_boost_kick(DEVFREQ_CPU_LLCC_DDR_BW);
+			break;
+		case 3:
+			cpu_input_boost_kick_max(100);
+			devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW, 100);
+			break;
+		default:
+			break;
+		}
 	}
 
 	drm_modeset_acquire_init(&ctx, 0);
