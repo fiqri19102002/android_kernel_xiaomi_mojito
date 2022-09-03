tzuyu-xd/android_kernel_xiaomi_ginkgo-2

drm: Boost CPU & DDR bus upon running an atomic ioctl
In order to reduce jank, request a DDR bus boost whenever an atomic
ioctl is sent, which is what our display HAL uses to commit frames to
the display. The boost should be sufficient enough to render the
display's maximum FPS without any dropped frames when there is no
significant external source of load.

Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
[fiqri19102002: Switch to CPUBW function and Implement CIB]
Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

drivers/gpu/drm/drm_atomic.c
@@ -30,6 +30,8 @@
 #include <drm/drm_atomic.h>
 #include <drm/drm_mode.h>
 #include <drm/drm_print.h>
+#include <linux/cpu_input_boost.h>
+#include <linux/devfreq_boost.h>
 #include <linux/pm_qos.h>
 #include <linux/sync_file.h>
 
@@ -2247,6 +2249,11 @@ static int __drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
 	if ((arg->flags & DRM_MODE_ATOMIC_TEST_ONLY) &&
 			(arg->flags & DRM_MODE_PAGE_FLIP_EVENT))
 		return -EINVAL;
+
+	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY)) {
+		cpu_input_boost_kick();
+		devfreq_boost_kick(DEVFREQ_CPU_CPU_DDR_BW);
+	}
 
 	drm_modeset_acquire_init(&ctx, 0);
 

fiqri19102002/android_kernel_xiaomi_mojito

drivers/gpu/drm/drm_atomic.c
@@ -30,6 +30,8 @@
 #include <drm/drm_atomic.h>
 #include <drm/drm_mode.h>
 #include <drm/drm_print.h>
+#include <linux/cpu_input_boost.h>
+#include <linux/devfreq_boost.h>
 #include <linux/pm_qos.h>
 #include <linux/sync_file.h>
 
@@ -2247,6 +2249,11 @@ static int __drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
 	if ((arg->flags & DRM_MODE_ATOMIC_TEST_ONLY) &&
 			(arg->flags & DRM_MODE_PAGE_FLIP_EVENT))
 		return -EINVAL;
+
+	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY)) {
+		cpu_input_boost_kick();
+		devfreq_boost_kick(DEVFREQ_CPU_LLCC_DDR_BW);
+	}
 
 	drm_modeset_acquire_init(&ctx, 0);
 