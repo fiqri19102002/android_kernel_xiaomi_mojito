3a7ce9de41ce5ab54b70de3029b1d6744906c2f2
Fiqri Ardyansyah <fiqri0927936@gmail.com>
2022-10-20 17:43:25 +0700

    mm: vmscan: Don't create debugfs when CONFIG_DEBUG_FS is disabled
    Signed-off-by: Fiqri Ardyansyah <fiqri0927936@gmail.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -53,7 +53,9 @@
 #include <linux/pagevec.h>
 #include <linux/shmem_fs.h>
 #include <linux/ctype.h>
+#ifdef CONFIG_DEBUG_FS
 #include <linux/debugfs.h>
+#endif
 
 #include <asm/tlbflush.h>
 #include <asm/div64.h>
@@ -4684,6 +4686,7 @@ static struct attribute_group lru_gen_attr_group = {
 	.attrs = lru_gen_attrs,
 };
 
+#ifdef CONFIG_DEBUG_FS
 /******************************************************************************
  *                          debugfs interface
  ******************************************************************************/
@@ -4787,11 +4790,7 @@ static void lru_gen_seq_show_full(struct seq_file *m, struct lruvec *lruvec,
 static int lru_gen_seq_show(struct seq_file *m, void *v)
 {
 	unsigned long seq;
-#ifdef CONFIG_DEBUG_FS
 	bool full = !debugfs_real_fops(m->file)->write;
-#else
-	bool full = false;
-#endif
 	struct lruvec *lruvec = v;
 	struct lru_gen_struct *lrugen = &lruvec->lrugen;
 	int nid = lruvec_pgdat(lruvec)->node_id;
@@ -5030,6 +5029,7 @@ static const struct file_operations lru_gen_ro_fops = {
 	.llseek = seq_lseek,
 	.release = seq_release,
 };
+#endif
 
 /******************************************************************************
  *                          initialization
@@ -5089,8 +5089,10 @@ static int __init init_lru_gen(void)
 	if (sysfs_create_group(mm_kobj, &lru_gen_attr_group))
 		pr_err("lru_gen: failed to create sysfs group\n");
 
+#ifdef CONFIG_DEBUG_FS
 	debugfs_create_file("lru_gen", 0644, NULL, NULL, &lru_gen_rw_fops);
 	debugfs_create_file("lru_gen_full", 0444, NULL, NULL, &lru_gen_ro_fops);
+#endif
 
 	return 0;
 };
