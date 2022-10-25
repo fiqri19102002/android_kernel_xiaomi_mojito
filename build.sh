7fd3fac97db4554c8e1407ae264ac184e66c2db8
Fiqri Ardyansyah <fiqri0927936@gmail.com>
2022-10-25 16:44:43 +0700

    build: Use LLVM=1 instead of define Clang/LLVM tools
    Signed-off-by: Fiqri Ardyansyah <fiqri0927936@gmail.com>

--- build.sh
+++ build.sh
@@ -129,12 +129,7 @@ compile() {
 	if [[ $COMPILER == "clang" ]]; then
 		make -j"$PROCS" O=out \
 				CROSS_COMPILE=aarch64-linux-gnu- \
-				CC=clang \
-				AR=llvm-ar \
-				NM=llvm-nm \
-				LD=ld.lld \
-				OBJDUMP=llvm-objdump \
-				STRIP=llvm-strip
+				LLVM=1
 	elif [[ $COMPILER == "gcc" ]]; then
 		export CROSS_COMPILE_ARM32=$GCC32_DIR/bin/arm-eabi-
 		make -j"$PROCS" O=out CROSS_COMPILE=aarch64-elf-
