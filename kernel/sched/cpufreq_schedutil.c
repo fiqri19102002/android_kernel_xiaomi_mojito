5c88bff8c8aaf8666a12e6c998dfc975adcf28de
Fiqri Ardyansyah <fiqri0927936@gmail.com>
2022-12-17 21:53:32 +0700

    schedutil: Reverse up/down ratelimits values
    This can improve sequential/random reads and writes significantly.
    Thanks to romgharti @ github for pointing this out.
    
    Signed-off-by: Fiqri Ardyansyah <fiqri0927936@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -21,8 +21,8 @@
 #include "sched.h"
 
 #define SUGOV_KTHREAD_PRIORITY	50
-#define UP_RATE_LIMIT_US		(20000)
-#define DOWN_RATE_LIMIT_US		(500)
+#define UP_RATE_LIMIT_US		(500)
+#define DOWN_RATE_LIMIT_US		(20000)
 
 struct sugov_tunables {
 	struct gov_attr_set attr_set;
