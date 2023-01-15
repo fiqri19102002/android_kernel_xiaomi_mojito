c0622cc9c2f38d8c57d3666fb98083ea320c6928
Fiqri Ardyansyah <fiqri0927936@gmail.com>
2023-01-15 22:35:06 +0700

    kernel: printk: Remove filter weird log spam from userspace
    We have workaround to silence spam logs from userspace.
    
    Signed-off-by: Fiqri Ardyansyah <fiqri0927936@gmail.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -809,10 +809,6 @@ static ssize_t devkmsg_write(struct kiocb *iocb, struct iov_iter *from)
 			endp++;
 			len -= endp - line;
 			line = endp;
-			if (strstr(line, "healthd") ||
-			    strstr(line, "cacert") ||
-			    strncmp(line, "logd: Skipping", sizeof("logd: Skipping")))
-				return ret;
 		}
 	}
 
