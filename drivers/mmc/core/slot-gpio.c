b54baf3ae9da05b31cd6638488f2a57d59c140c3
Fiqri Ardyansyah <fiqri0927936@gmail.com>
2023-05-03 16:42:10 +0700

    mmc: slot-gpio: Fix unused result on mmc_gpiod_restore_cd_irq()
    Signed-off-by: Fiqri Ardyansyah <fiqri0927936@gmail.com>

--- drivers/mmc/core/slot-gpio.c
+++ drivers/mmc/core/slot-gpio.c
@@ -141,10 +141,11 @@ EXPORT_SYMBOL(mmc_gpiod_free_cd_irq);
 void mmc_gpiod_restore_cd_irq(struct mmc_host *host)
 {
 	struct mmc_gpio *ctx = host->slot.handler_priv;
-	int irq = host->slot.cd_irq;
+	int ret, irq;
 
+	irq = host->slot.cd_irq;
 	if (irq >= 0) {
-		devm_request_threaded_irq(host->parent, irq,
+		ret = devm_request_threaded_irq(host->parent, irq,
 			NULL, ctx->cd_gpio_isr,
 			IRQF_TRIGGER_RISING | IRQF_TRIGGER_FALLING |
 			IRQF_ONESHOT,
